e3c3c8bf7c2ba73e58a1527e32d3c1e9620cf788
Matheus Vicente <mv_vieira96@hotmail.com>
2023-11-18 11:33:02 -0300

    Feature - Criação do Repository de Ocorrência

---
+++ src/main/java/com/br/healthtech/infra/repository/OcorrenciaRepository.java
@@ -0,0 +1,9 @@
+package com.br.healthtech.infra.repository;
+
+import com.br.healthtech.domain.entity.Ocorrencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OcorrenciaRepository extends JpaRepository<Ocorrencia, Integer> {
+}
