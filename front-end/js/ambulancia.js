30c27b42f5cbdbb1728d75bdbf3464136130621c
Matheus Vicente <mv_vieira96@hotmail.com>
2023-11-24 19:44:09 -0300

    Inserção de textos no html who, mudando sort da requisição de ocorrência e realizando pequenas alterações no css

--- front-end/js/ambulancia.js
+++ front-end/js/ambulancia.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const tableBody = document.getElementById('table-body');
     const tableTitle = document.getElementById('table-title');
-    const ApiUrl = 'http://localhost:8080/health-tech/ambulancias/buscarplaca?placa=KJZ-9239' // KGT-0357
+    const ApiUrl = 'http://localhost:8080/health-tech/ambulancias/buscarplaca?placa=KJZ-9239' // KGT-0357 //KJZ-9239
 
     // Realiza a requisição à API
     fetch(ApiUrl)
         .then(response => response.json())
         .then(data => {
             console.log(data)
-            data.ocorrencias.sort((a, b) => new Date(a.dataHora) - new Date(b.dataHora));
+            data.ocorrencias.sort((a, b) => new Date(b.dataHora) - new Date(a.dataHora));
 
             // Limpa o corpo da tabela
             tableBody.innerHTML = '';
@@ -38,12 +38,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // Função para formatar a data e hora para o padrão brasileiro
 function formatarDataHora(dataHora) {
-    const options = { day: 'numeric', month: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZone: 'UTC' };
+    const options = { day: 'numeric', month: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZone: 'America/Sao_Paulo'};
     return new Intl.DateTimeFormat('pt-BR', options).format(new Date(dataHora));
 }
 
 // Formatar data de nascimento
 function formatarDataNascimento(dataNascimento) {
-    const options = { day: 'numeric', month: 'numeric', year: 'numeric', timeZone: 'America/Sao_Paulo' };
+    const options = { day: 'numeric', month: 'numeric', year: 'numeric'};
     return new Intl.DateTimeFormat('pt-BR', options).format(new Date(dataNascimento));
 }
