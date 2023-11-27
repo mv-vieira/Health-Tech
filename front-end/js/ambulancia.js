f4f8518d5426f5c8b27dbc094700f876fc2b5fa7
Matheus Vicente <mv_vieira96@hotmail.com>
2023-11-27 19:34:58 -0300

    Feature - Implementando busca de ocorrências através da placa da ambulância

--- front-end/js/ambulancia.js
+++ front-end/js/ambulancia.js
@@ -1,24 +1,44 @@
-document.addEventListener('DOMContentLoaded', () => {
+function removePlaceHolder(input) {
+    input.placeholder = "";
+}
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('search-button').addEventListener('click', procurarPlaca);
+});
+
+function procurarPlaca() {
+    let placa = document.getElementById("placaInput").value;
     const tableBody = document.getElementById('table-body');
     const tableTitle = document.getElementById('table-title');
-    const ApiUrl = 'http://localhost:8080/health-tech/ambulancias/buscarplaca?placa=KJZ-9239' // KGT-0357 //KJZ-9239
+    tableBody.innerHTML = '';
+    const ApiUrl = `http://localhost:8080/health-tech/ambulancias/buscarplaca?placa=${placa}`;
 
-    // Realiza a requisição à API
     fetch(ApiUrl)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            data.ocorrencias.sort((a, b) => new Date(b.dataHora) - new Date(a.dataHora));
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Erro na requisição: ${response.status}`);
+        }
+        const contentType = response.headers.get('content-type');
+        if (contentType && contentType.includes('application/json')) {
+            return response.json();
+        } else {
+            throw new TypeError('A resposta não é JSON válido');
+        }
+    })
+    .then(data => {
+        console.log(data);
+        tableTitle.innerHTML = `Ocorrências da Ambulância - Placa ${data.placaAmbulancia}`;
 
-            // Limpa o corpo da tabela
-            tableBody.innerHTML = '';
-            tableTitle.innerHTML = `Ocorrências da Ambulância - Placa ${data.placaAmbulancia}`;
+        // Faz uma cópia do array e ordena
+        const sortedOcorrencias = [...data.ocorrencias].sort((a, b) => new Date(b.dataHora) - new Date(a.dataHora));
 
+        // Usar um conjunto para evitar duplicatas
+        const uniqueRows = new Set();
 
-            // Preenche a tabela com os dados da API
-            data.ocorrencias.forEach(ocorrencia => {
+        // Preenche a tabela com os dados ordenados da API
+        sortedOcorrencias.forEach(ocorrencia => {
+            const rowKey = `${ocorrencia.protocolo}-${ocorrencia.paciente.nome}`;
+            if (!uniqueRows.has(rowKey)) {
                 const row = document.createElement('tr');
-                
 
                 row.innerHTML = `
                     <td>${ocorrencia.protocolo}</td>
@@ -26,15 +46,21 @@ document.addEventListener('DOMContentLoaded', () => {
                     <td>${formatarDataNascimento(ocorrencia.paciente.dataNascimento)}</td>
                     <td>${ocorrencia.endereco}</td>
                     <td>${formatarDataHora(ocorrencia.dataHora)}</td>
-                    <td>${ocorrencia.hospital.nomeHospital + ", "+ ocorrencia.hospital.municipio}</td>
+                    <td>${ocorrencia.hospital.nomeHospital + ", " + ocorrencia.hospital.municipio}</td>
                 `;
 
                 tableBody.appendChild(row);
-            });
-            
-        })
-        .catch(error => console.error('Erro na requisição:', error));
-});
+
+                // Adiciona a chave ao conjunto para evitar duplicatas
+                uniqueRows.add(rowKey);
+            }
+        });
+
+        console.log('Finalizando procurarPlaca...');
+    })
+    .catch(error => console.error('Erro na requisição:', error));
+}
+
 
 // Função para formatar a data e hora para o padrão brasileiro
 function formatarDataHora(dataHora) {
